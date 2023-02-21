60a0e06d5a2e802aa7a6a1fae63da2bbfe2554d1
fatihyanik <fatihyanik07@gmail.com>
2023-02-21 16:03:31 +0100

    inital commit

---
+++ randomnumber/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='container'>
+          <div className='randomNum'>
+
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
