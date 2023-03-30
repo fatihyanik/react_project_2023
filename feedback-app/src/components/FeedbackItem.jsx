82d30ce724675c09beaa0ac68afbef90e5d1071c
fatihyanik <fatihyanik07@gmail.com>
2023-03-30 16:06:40 +0200

    added feedbackstats

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,13 +1,26 @@
-function FeedbackItem({item}) {
-    console.log(item);
+import Card from "./shared/Card";
+import{ FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types';
+
+function FeedbackItem({item, handleDelete}) {
+    //console.log(item);
+
+
   return (
-    <div className="card">
+    <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={()=> handleDelete(item.id)} className="close">
+            <FaTimes color='purple' />
+        </button>
         <div className="text-display">
             {item.text}
         </div>
-    </div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
     //console.log(feedback);
 
     if (!feedback || feedback.length === 0) {
@@ -11,10 +12,26 @@ function FeedbackList({ feedback }) {
         <div className="feedback-list">
             {
                 feedback.map((item) => {
-                    return (<FeedbackItem key={item.id} item={item} />)
+                    return (
+                        <FeedbackItem
+                            key={item.id}
+                            item={item}
+                            handleDelete={handleDelete}
+                        />
+                    )
                 })
             }
         </div>
+    )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
     )
 }
 
