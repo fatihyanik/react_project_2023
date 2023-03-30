690d30b1ceb6debc49fe3d961eebf2e280b42d17
fatihyanik <fatihyanik07@gmail.com>
2023-03-30 13:41:53 +0200

    added state, props

---
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+function FeedbackItem({item}) {
+    console.log(item);
+  return (
+    <div className="card">
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">
+            {item.text}
+        </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
