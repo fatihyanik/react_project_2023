442151266cb8829f0ce1c0d85e731ef74dd5ee4c
fatihyanik <fatihyanik07@gmail.com>
2023-04-14 21:52:12 +0200

    youtubeform2

--- react_hook_form/src/App.tsx
+++ react_hook_form/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import YouTubeForm from './components/YouTubeForm'
+import YouTubeForm1 from './components/YouTubeForm1'
+import YouTubeForm2 from './components/YouTubeForm2'
 
 function App() {
 
   return (
     <div>
-      <YouTubeForm/>
+      {/* <YouTubeForm /> */}
+      {/* <YouTubeForm1 /> */}
+      <YouTubeForm2 />
     </div>
   )
 }
